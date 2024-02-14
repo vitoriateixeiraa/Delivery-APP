1270b05a5556dcc78c11cf75474e0176c159a2a6
Vitória Teixeira <teixeiravitoria057@gmail.com>
2024-02-14 19:40:35 -0300

    first commit

---
+++ src/components/button.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from "react";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Text } from "react-native";
+
+
+type ButtonProps = TouchableOpacityProps & {
+  children: ReactNode;
+};
+
+type ButtonTextProps = {
+    children: ReactNode
+}
+
+type ButtonIconProps = {
+    children: ReactNode
+}
+
+
+function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity
+      className="h-12 bg-lime-400 rounded-s-md items-center justify-center flex-row"
+      activeOpacity={0.7}
+      {...rest}
+    >
+      {children}
+    </TouchableOpacity>
+  );
+}
+
+
+function ButtonText({ children }: ButtonTextProps){
+    return(
+        <Text className="text-black font-heading text-base mx-2">{children}</Text>
+    )
+
+}
+
+function ButtonIcon({ children }: ButtonIconProps){
+    return children
+}
+
+Button.Text = ButtonText
+Button.Icon = ButtonIcon
+
+export { Button }
---
+++ src/components/link-button.tsx
@@ -0,0 +1,13 @@
+import { Link, LinkProps } from "expo-router";
+
+type linkButtonProps = LinkProps<string> & {
+    title: string
+}
+
+export function LinkButton({title, ...rest}: linkButtonProps){
+    return (
+        <Link className="text-slate-300 text-center text-base font-body" {...rest}>
+            {title}
+        </Link>
+    )
+}
